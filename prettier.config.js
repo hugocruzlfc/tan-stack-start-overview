79402299e17fa96188834d7f23e24720c1a017f9
Hugo Cruz <hugocruzlfc@gmail.com>
2026-02-19 14:29:30 +0100

    chore: add prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,12 @@
 const config = {
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
+}
 
-export default config;
+export default config
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
 
 const config = defineConfig({
   plugins: [
